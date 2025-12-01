5f36f90ef1f01ce9050e4d6b3a40ca5207ad159e
Pristine duru <Favourduru24@gmail.com>
2025-12-01 07:45:59 -0800

    done implementing the register auth function

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,50 @@
+import type { LoginType, RegisterType, UserType } from '@/types/auth.type'
+import {create} from 'zustand'
+import {persist} from "zustand/middleware"
+import {toast} from "sonner"
+import { API } from '@/lib/utils'
+import { useSocket } from './use-socket'
+
+interface AuthState {
+    user: UserType | null
+    isLoggingIn: boolean
+    isSigningUp: boolean
+    isAuthStateLoading: boolean
+
+    register: (data: RegisterType) => void
+    login: (data: LoginType) => void
+    logout: () => void,
+    isAuthStatus: () => void
+} 
+
+
+ export const useAuth = create<AuthState>()(
+    persist(
+       (set) => ({
+         user: null,
+         isSigningUp: false,
+         isLoggingIn: false,
+         isAuthStateLoading: false,
+         
+         register: async (data: RegisterType) => {
+            set({isSigningUp: true})
+
+            try {
+
+                const response = await API.post('/auth/register', data)
+                set({user: response.data.user})
+                useSocket.getState().connectSocket()
+            } catch (err: any) {
+                toast.error(err.response?.data?.message || 'Registration failed!')
+            } finally {
+                set({isSigningUp: false})
+            }
+         },
+         login: (data: LoginType) => {},
+         logout: () => {},
+         isAuthStatus: () => {}
+       }),
+       {
+         name: 'whop:root'
+       }
+    ))
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const API = axios.create({
+   baseURL: import.meta.env.MODE === 'development' 
+   ? `${import.meta.env.VITE_API_URL}/api`
+   : '/api',
+   withCredentials: true
+})
