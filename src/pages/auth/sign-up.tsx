d33b627476dbf79833572056e86dd01d940dcbdd
Pristine duru <Favourduru24@gmail.com>
2025-12-02 04:10:33 -0800

    new code update

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -4,7 +4,7 @@ const SignUp = () => {
 
   return (
     <section className="w-full flex justify-center items-center h-screen">
-         <div className="w-full max-w-lg flex flex-col gap-6 py-6 bg-white border border-[#f9fbfc] rounded-xl shadow-100 p-2 justify-center">
+         <div className="w-full max-w-lg flex flex-col gap-6 py-6 bg-white border border-[#ebeeed] rounded-xl shadow-sm p-2 justify-center">
                               <div className="w-full flex flex-col gap-3">
                                 <div className="flex gap-2 justify-center items-center">
                                   <div className="w-8 h-8 rounded-md bg-purple-400 flex items-center justify-center">
@@ -19,22 +19,22 @@ const SignUp = () => {
               <form className="flex flex-col gap-4">
                  <div className="w-full flex flex-col gap-2.5 px-6 relative">
                      <label className="text-sm font-normal text-gray-400">Name</label>
-               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300" type="name" id="name" placeholder="Username"/>
+               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300 font-sans font-normal outline-purple-600" type="name" id="name" placeholder="Username"/>
                  </div>
 
                  <div className="w-full flex flex-col gap-2.5 px-6 relative">
                      <label className="text-sm font-normal text-gray-400">Email</label>
-               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300" type="email" id="email" placeholder="email@gmail.com"/>
+               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300 font-sans font-normal outline-purple-600" type="email" id="email" placeholder="email@gmail.com"/>
                  </div>
 
                  <div className="w-full flex flex-col gap-2.5 px-6 relative">
                      <label className="text-sm font-normal text-gray-400">Password</label>
-               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300" type="password" id="password" placeholder="pa**word"/>
+               <input className="p-3 ring-2 ring-[#f9fbfc] rounded-md placeholder:text-gray-300 font-sans font-normal outline-purple-600" type="password" id="password" placeholder="pa**word"/>
                  </div>
 
                   <div className="w-full flex flex-col gap-2.5 px-6 relative mt-4">
-                      <button className="w-full bg-purple-800 rounded-md">
-                        <p className="text-white font-semibold text-xl p-2 ">Sign In</p>
+                      <button className="w-full bg-purple-800 hover:bg-purple-700 focus:bg-purple-500 rounded-md cursor-pointer">
+                        <p className="text-white font-semibold text-xl p-2 ">Sign Up</p>
                       </button>
                  </div>
               </form>
