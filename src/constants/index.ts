6b03e4bdfbb20d844673c059c6f84d94e4695274
Duru Favour <favourduru24@gmail.com>
2025-12-08 01:41:29 -0800

    new code update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,3 +8,18 @@ import { Calendar, Sparkle } from "lucide-react";
       icon: Sparkle
     },
    ]
+
+   export const chatData = [
+          {name: 'Favour Duru', text: 'I have sent you the link', time: '9:53', color: 'bg-red-500', },
+          {name: 'Duru Pristine', text: 'How should i start', time: '7:53', color: 'bg-purple-500'},
+          {name: 'Kelvin Kart', text: 'I have been working', time: '9:53', color: 'bg-green-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '10:53', color: 'bg-pink-500'},
+          {name: 'Favour Duru', text: 'I have sent you the link', time: '1:53', color: 'bg-black'},
+          {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
+          {name: 'Kelvin Kart', text: 'I have been working', time: '12:53', color: 'bg-yellow-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
+          {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
+   ]
